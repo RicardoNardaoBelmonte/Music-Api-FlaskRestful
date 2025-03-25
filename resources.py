5bbd52eaf30df093658246633ca017f21e8d5658
RicardoNardaoBelmonte <nardaoricardo@gmail.com>
2025-03-25 11:17:03 -0300

    resources

---
+++ resources.py
@@ -0,0 +1,61 @@
+from flask_restful import Resource, reqparse, fields, marshal_with
+from .models import Music
+from database import db
+
+#defining the arguments expected in the request
+music_args = reqparse.RequestParser()
+music_args.add_argument("artist", type=str, required=True, help='The field Artist is required')
+music_args.add_argument("genre", type=str, required=True, help='The field Genre is required')
+music_args.add_argument("year", type=int, required=False, help="The field year is not required it's optional")
+
+#defining the structure of the response
+resource_fields = {
+    "id": fields.Integer,
+    "artist": fields.String,
+    "genre": fields.String,
+    "year": fields.Integer
+}
+
+class MusicResource(Resource):
+    @marshal_with(resource_fields)
+    def get_all(self):
+        musics = Music.query.all()
+        return musics, 200
+
+
+    @marshal_with(resource_fields)
+    def get(self, music_id):
+        music = Music.query.get(music_id)
+        if not music:
+            return {"massage": "Music not found"}, 404
+        return music, 200
+    
+    @marshal_with(resource_fields)
+    def post(self):
+        data = music_args.parse_args()
+        music = Music(artist=data['artist'], genre=data['genre'], year=data['year'])
+        db.session.add(music)
+        db.session.commit()
+        return music, 201
+
+
+    def delete(self, music_id):
+        music = Music.query.get(music_id)
+        if not music:
+            return {"message": "Music not Found"}, 404
+        db.session.delete(music)
+        db.session.commit()
+        return {"message": "Music deleted"}, 204
+
+    @marshal_with(resource_fields)
+    def put(self, music_id):
+        data = music_args.parse_args() 
+        music = Music.query.get(music_id)
+        if not music:
+            return {"message": "Music not found"},404
+        
+        music.artist = data['artist']
+        music.genre = data['genre']
+        music.year = data['year']
+        db.session.commit()
+        return  music, 200
