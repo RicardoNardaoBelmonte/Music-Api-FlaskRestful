91017aade316b0352f60f769cc35d1c5f7ffc5bb
RicardoNardaoBelmonte <nardaoricardo@gmail.com>
2025-03-25 13:56:30 -0300

    models fixed

--- models.py
+++ models.py
@@ -2,7 +2,10 @@ from database import db
 
 #criando a tabela Music com seus respectivos campos para o banco de dados
 class Music(db.Model):
-    id = db.Colummn(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     artist = db.Column(db.String(50), nullable=False)
     genre = db.Column(db.String(50), nullable=False)
     year = db.Column(db.Integer, nullable=False)
+    
+    def __repr__(self):
+        return f'<Music {self.artist} - {self.genre} - {self.year}>'
