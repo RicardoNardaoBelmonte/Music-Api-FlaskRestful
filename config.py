e0a58bb1e4e5c2c64e67d17a296fbc66dd9ba617
RicardoNardaoBelmonte <nardaoricardo@gmail.com>
2025-03-25 15:32:30 -0300

    configs

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///music.db' #name of bdd (caminho)
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///music.db") #name of bdd (caminho)
     SQLALCHEMY_TRACK_MODIFICATIONS = False # disable track modifications off (rastreamente de modificações)
