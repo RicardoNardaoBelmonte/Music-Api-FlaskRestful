9a3e1fa97fdd848a8d4e954c99d08ac0bf992c52
RicardoNardaoBelmonte <nardaoricardo@gmail.com>
2025-04-03 15:27:07 -0300

    adding tests for aplication

--- tests/test_music_api.py
+++ tests/test_music_api.py
@@ -20,3 +20,42 @@ def test_get_nonexistent_music(test_client):
     response = test_client.get('/music/999')
     assert response.status_code==404
 
+def test_update_music (test_client, created_music_id):
+    #testing put method
+    update_data = {
+        'artist': 'Eminem',
+        'genre': 'rap',
+        'year': 2000
+    }
+
+    response = test_client.put(''
+        f'/music/{created_music_id}',
+        json=update_data
+    )
+    
+    assert response.status_code == 200
+    update_music = response.json
+    for key in update_data:
+        assert update_music[key] == update_data[key]#comparing data
+
+def test_delete_music(test_client, created_music_id):
+    #this is a test for delete music
+    #first we make sure the id exist
+    get_response = test_client.get(f'/music/{created_music_id}')
+    assert get_response.status_code == 200
+
+    #doing the delete
+    delete_response = test_client.delete(f'/music/{created_music_id}')
+    assert delete_response.status_code == 204
+
+    get_response = test_client.get(f'/music/{created_music_id}')
+    assert get_response.status_code == 404#and here we are trying to return again the id to make sure it was deleted
+
+def test_get_all_musics(test_client, created_music_id):
+    #testing get all musics
+    response = test_client.get('/musics')
+
+    assert response.status_code == 200
+    assert isinstance(response.json, list)
+    assert len(response.json) > 0
+    assert any(music['id'] == created_music_id for music in response.json)# this is a very cool way to verify if the response are right with comparing the ids 
