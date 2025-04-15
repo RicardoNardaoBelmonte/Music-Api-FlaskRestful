14e220d6b7ca6a81c9d163365c5c0bed03862f0a
RicardoNardaoBelmonte <nardaoricardo@gmail.com>
2025-04-15 10:20:20 -0300

    adding migrate object

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask_restful import Api
 from config import Config # importing class config
 from database import configure_bdd
 from resources import MusicResource, MusicListResource
+from flask_migrate import Migrate 
+from database import db
 
 app = Flask(__name__)
 app.config.from_object(Config) # setting the configuration of the app(bdd)
 configure_bdd(app) #configuring the database( is calling the function to create the tables in bdd)
+migrate = Migrate(app, db)# creating the migration object to manage the migrations of the database
 
 api = Api(app) 
 api.add_resource(MusicResource, '/music', '/music/<int:music_id>') # ading the resource to api
